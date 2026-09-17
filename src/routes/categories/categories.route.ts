81f3ef03f9b994a65f419d01512d1766e049f5ce
Septia <septiar436@gmail.com>
2026-09-18 00:23:36 +0700

    chore: separate feature files from base

--- src/routes/categories/categories.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import CategoryController from "../../controllers/categories/category.controller";
-
-const router = Router();
-
-router.get("/", CategoryController.getCategories);
-
-export default router;
